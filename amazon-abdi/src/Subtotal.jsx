97fdacfb6f17ee34d1a9bc995e1504d76cfafbfd
abdisa awel <abdisaawel82@gmail.com>
2025-02-25 10:54:29 -0800

    amazon

--- amazon-abdi/src/Subtotal.jsx
+++ amazon-abdi/src/Subtotal.jsx
@@ -1,15 +1,25 @@
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
 import "./subtotal.css"
 import CurrencyFormat from "react-currency-format"
 
 
 const Subtotal = () => {
+
+const [{basket}, dispatch] = useStateValue();
+
+
+
+
+
+
   return (
     <div className = "subtotal">
          <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal-gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,7 +27,7 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0} 
+        value={getBasketTotal(basket)} 
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
